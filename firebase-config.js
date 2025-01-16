317617424c7fde7d5fcd2f7b7e024089795afffe
danieldfsm <danieldfsm9@outlook.com>
2025-01-16 02:15:46 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBmEcBQ8zMsgLAiwyR_NfMsG7JBrQFDerM",
+  authDomain: "rifas-web-4849a.firebaseapp.com",
+  projectId: "rifas-web-4849a",
+  storageBucket: "rifas-web-4849a.firebasestorage.app",
+  messagingSenderId: "754375136052",
+  appId: "1:754375136052:web:71cb451c2f8f3194ed4b61",
+  measurementId: "G-ZYENGDTYTB"
+};
